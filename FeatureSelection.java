776c21ec57137fa0be6ea8a49efa1d64934f81ef
Yashwanth <97561730+yashwanth-eshwarappa@users.noreply.github.com>
2022-06-10 01:54:22 -0700

    Update FeatureSelection.java
    Changes to data storage

--- FeatureSelection.java
+++ FeatureSelection.java
@@ -10,56 +10,52 @@ import java.util.Scanner;
 
 class FeatureSelection {
 	public static void main (String args[]) throws FileNotFoundException {
-		String filePath;
+		String filePath = "src/main/CS205_SP_2022_SMALLtestdata__9.txt";;
+		
 		if(args.length>0) {
 			filePath = args[0];
 		}
-		else {
-			filePath = "src/main/CS205_SP_2022_SMALLtestdata__9.txt";
-		}
+//		else {
+//			filePath = "src/main/CS205_SP_2022_SMALLtestdata__9.txt";
+//		}
+		
 		Scanner sc = new Scanner(new File(filePath));
-		
-		
-		
-		Map<Double, List<List<Double>>> map = new HashMap<>();
-//		type.put((double) 0, new ArrayList<>());
-//		type.put((double) 1, new ArrayList<>());
+		List<List<Double>> dataList = new ArrayList<>();
 		
 		
 		while(sc.hasNext()) {
-//			String[] eachLine = sc.nextLine().split("   ");
 			
 			Scanner scEachLine = new Scanner(sc.nextLine());
+//			Double type = scEachLine.nextDouble();
 			
-			Double type = scEachLine.nextDouble();
-//			map.put(type, new ArrayList<>());
 			List<Double> features = new ArrayList<>();
 			
 			while(scEachLine.hasNext()){
 				features.add(scEachLine.nextDouble());
 			}
 			
-			if(!map.containsKey(type)) {
-				map.put(type, new ArrayList<>());
-				
-			}
-			map.get(type).add(features);
-			
-//			System.out.println(eachLine[1]);
-//			List<Double> features = new ArrayList<>();
-//			type.put((Double.parseDouble(eachLine[1])), new ArrayList<>());
-//			for(int i=2;i<eachLine.length;i++) {
-//				System.out.println(eachLine[i]);
-////				type.get(Double.parseDouble(eachLine[1])).add(Double.parseDouble(eachLine[i]));
+			dataList.add(features);
+//			if(!map.containsKey(type)) {
+//				map.put(type, new ArrayList<>());
+//				
 //			}
-////			type.get(eachLine[0]).(features);
-			
-//			System.out.println(eachLine[1]);
-//			break;
+//			map.get(type).add(features);
 		}
 		
-		for(Map.Entry<Double, List<List<Double>>> each: map.entrySet()) {
-			System.out.println(each.getValue());
-		}
+//		Iterator<Double> it = dataList.iterator();
+		
+		System.out.println(dataList.get(0));
+		
+//		while(it.hasNext()) {
+//			System.out.println(it.get());
+//		}
+		
+//		//checking data
+//		for(Map.Entry<Double, List<List<Double>>> each: map.entrySet()) {
+//			System.out.println("Key:" + each.getKey() + ", value:" + each.getValue());
+//		}
+		
+		
+		forwardSelection(dataList);
 	}
 }
