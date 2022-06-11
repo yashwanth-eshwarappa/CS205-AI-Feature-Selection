997c59b61f692887e08d050437715de315130f31
Yashwanth <97561730+yashwanth-eshwarappa@users.noreply.github.com>
2022-06-10 19:45:44 -0700

    Update FeatureSelection.js
    Refactored and added code comments.

--- FeatureSelection.js
+++ FeatureSelection.js
@@ -1,57 +1,102 @@
 const fs = require('fs');
 const readline = require('readline');
 
-// Creating a readable stream from file
-// readline module reads line by line 
-// but from a readable stream only.
-// const file = readline.createInterface({
-//     input: fs.createReadStream(process.cwd() + '/CS205_CalibrationData__1.txt'),
-//     output: process.stdout,
-//     terminal: false
-// });
+// Read input from user
+const inputRead = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+const args = process.argv;
+
+// Storing the dataset in the list
 var dataList = [];
 
+console.log("--------------------------");
+console.log("Feature Selection Project");
+console.log("--------------------------");
+
+inputRead.question('Select the search option from below:\n' +
+    '1. Forward Selection \n' +
+    '2. Backward Elimination\n', selection => {
+        if (selection == 1) {
+            console.log("\nSelected search: Forward Selection\n");
+        }
+        if (selection == 2) {
+            console.log("\nSelected search: Backward Elimination\n");
+        }
+
+        // Reading the data from the provided file
+        readData().then(() => {
+            if (selection == 1) {
+                let start = Date.now();
+                //Calling Forward selection upon reading the dataset
+                forwardSelection(dataList);
+                let end = Date.now();
+                console.log("Total Time taken = " + Number.parseFloat(end - start));
+            }
+            else if (selection == 2) {
+                let start = Date.now();
+                forwardSelection(dataList);
+                //Calling Backward elimination upon reading the dataset
+                backwardElimination(dataList);
+                let end = Date.now();
+                console.log("Total Time taken = " + Number.parseFloat(end - start));
+            }
+            else {
+                console.log("Incorrect search selected!!");
+            }
+
+        });
+        inputRead.close();
+    });
+
+// Reading the data points from file and storing in the list dataList
 async function readData() {
-    const fileStream = fs.createReadStream(process.cwd() + '/CS205_CalibrationData__1.txt');
+    var filePath;
+    if (args[2] != null) {
+        filePath = args[2];
+    }
+    else {
+        filePath = process.cwd() + '/CS205_SP_2022_SMALLtestdata__10.txt'
+    }
+    const fileStream = fs.createReadStream(filePath);
 
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
     });
+    // Iterating each line and extracting the data
     for await (var line of rl) {
         var list = [];
         for (var each of line.trim().split(/\s+/)) {
             list.push(Number.parseFloat(each));
         }
+        // Adding all data points to dataList
         dataList.push(list);
     }
 }
 
 function forwardSelection(dataList) {
-    var returnString = "<h2><u>Forward Selection</u></h2>";
-    console.log(dataList[0].length);
+    // Count of features
     var numOfFeature = dataList[0].length;
-
-    console.log(numOfFeature - 1);
-
+    // Flag to check if the feature already visited for the same entry
     var featuresVisited = [];
     var selectedFeatures = [];
+    // storing the best feature for forward selection in this list
     var finalBestAttributes = [];
+    // To store the maximum accuracy value for selected features
     var maxAccuracy = Number.MIN_VALUE;
 
-    //taking each feature 
+    // Iterate each feature of datapoint
     for (var j = 1; j < numOfFeature; j++) {
-
-        var bestAccurancyThisTurn = 0;
-        var bestAttributeToAdd = 0;
+        var currBestAccuracy = 0;
+        var currBestFeature = 0;
 
         for (var i = 1; i < numOfFeature; i++) {
 
+            // Saving the selected features after each iteration to current selection
             var localSelectedFeatures = [];
-
-            // console.log(selectedFeatures);
-
             localSelectedFeatures = [...selectedFeatures];
 
             if (featuresVisited[i]) {
@@ -60,48 +105,51 @@ function forwardSelection(dataList) {
 
             localSelectedFeatures.push(i);
 
+            // Calculating the accuracy by checking each localSelectedFeatures
             var correctlyPredictedTypeCount = 0;
             for (var eachdata = 0; eachdata < dataList.length; eachdata++) {
                 if (calculateAccuracy(eachdata, dataList, localSelectedFeatures)) {
                     correctlyPredictedTypeCount++;
                 }
             }
+            // Get the accuracy by dividing the correctlyPredictedTypeCount with total data entries of all types
             var result = (correctlyPredictedTypeCount) / dataList.length;
 
-            // console.log(localSelectedFeatures);
-            returnString += "For the features " + (localSelectedFeatures.toString()) + " the accuracy is " + result + "<br>";
-            // console.log("->with features " + (localSelectedFeatures.toString()) + " the accuracy is " + result);
+            console.log("For the features " + (localSelectedFeatures.toString()) + " the accuracy is " + result);
 
+            // saving maximum accuracy for all the feature so far
             var temp = maxAccuracy;
             maxAccuracy = Math.max(result, maxAccuracy);
 
             if (temp < maxAccuracy) {
-                // finalBestAttributes.clear();
                 finalBestAttributes = localSelectedFeatures;
             }
-
-            if (result > bestAccurancyThisTurn) {
-                bestAccurancyThisTurn = result;
-                bestAttributeToAdd = i;
+            // saving the current iteration's best accuracy for features
+            if (result > currBestAccuracy) {
+                currBestAccuracy = result;
+                currBestFeature = i;
             }
         }
-
-        selectedFeatures.push(bestAttributeToAdd);
-        featuresVisited[bestAttributeToAdd] = true;
-        returnString += "<b>Selecting best features " + (selectedFeatures) + " with accuracy " + bestAccurancyThisTurn + "</b><br>";
-        console.log("Selecting best features " + (selectedFeatures) + " with accuracy " + bestAccurancyThisTurn);
+        // adding current best feature to selectedFeatures list which has best accuracy so far
+        selectedFeatures.push(currBestFeature);
+        // setting visited flag for current feature
+        featuresVisited[currBestFeature] = true;
+        console.log("Selecting best features " + (selectedFeatures) + " with accuracy " + currBestAccuracy);
     }
-    returnString += "<h3><b>Final best feature for forward selection is " + (finalBestAttributes) + " and the accuracy is " + maxAccuracy + "</b></h3><br>";
-    console.log("Final best feature for forward selection is " + (finalBestAttributes) + " and the accuracy is " + maxAccuracy);
 
-    return returnString;
+    maxAccuracy *= 100;
+    console.log("----------------------------------------------------------------------------------------");
+    console.log("Final best features for forward selection are " + (finalBestAttributes) + " and the accuracy is " + (maxAccuracy).toFixed(2) + "%");
+    console.log("----------------------------------------------------------------------------------------");
 }
 
 function backwardElimination(dataList) {
-    var returnString = "<h2><u>Backward Elimination</u></h2>";
+    // To store the maximum accuracy value for selected features
     var maxAccuracy = Number.MIN_VALUE;
+    // storing the best feature for forward selection in this list
     var finalBestAttributes = [];
     var numOfFeature = dataList[0].length;
+    // Flag to check if the feature already visited for the same entry
     var featuresVisited = [];
 
     var selectedFeatures = [];
@@ -109,18 +157,22 @@ function backwardElimination(dataList) {
         selectedFeatures.push(i);
     }
     for (var j = 0; j < numOfFeature; j++) {
-        var bestAccurancyThisTurn = 0;
-        var bestAttributeToAdd = 0;
+        var currBestAccuracy = 0;
+        var currBestFeature = 0;
         for (var i = 1; i < numOfFeature; ++i) {
+
+            // Saving the selected features after each iteration to current selection
             var localSelectedFeatures = [];
             localSelectedFeatures = [...selectedFeatures];
             if (featuresVisited[i]) {
                 continue;
             }
+            // skipping removal for the first iteration
             if (j != 0) {
                 localSelectedFeatures = removeElementFromList(localSelectedFeatures, i);
             }
 
+            // Calculating the accuracy by checking each localSelectedFeatures
             var correctlyPredictedTypeCount = 0;
             for (var eachdata = 0; eachdata < dataList.length; eachdata++) {
                 if (calculateAccuracy(eachdata, dataList, localSelectedFeatures)) {
@@ -129,108 +181,70 @@ function backwardElimination(dataList) {
             }
             var result = (correctlyPredictedTypeCount) / dataList.length;
 
-            // var result = testAccurancy(localSelectedFeatures);
+            // saving maximum accuracy for all the feature so far
             var temp = maxAccuracy;
             maxAccuracy = Math.max(result, maxAccuracy);
 
             if (temp < maxAccuracy) {
-                // finalBestAttributes.clear();
                 finalBestAttributes = localSelectedFeatures;
             }
 
+            console.log("For the features " + (localSelectedFeatures) + " the accuracy is " + result);
 
-            returnString += "For the features " + (localSelectedFeatures) + " the accuracy is " + result + "<br>";
-            // console.log("-->with features " + (localSelectedFeatures) + " the accuracy is " + result);
-            if (result > bestAccurancyThisTurn) {
-                bestAccurancyThisTurn = result;
-                bestAttributeToAdd = i;
+            // saving the current iteration's best accuracy for features
+            if (result > currBestAccuracy) {
+                currBestAccuracy = result;
+                currBestFeature = i;
             }
             if (j == 0) {
                 break;
             }
         }
-
+        // skipping removal for the first iteration
         if (j != 0) {
-            selectedFeatures = removeElementFromList(selectedFeatures, bestAttributeToAdd);
-            featuresVisited[bestAttributeToAdd] = true;
+            selectedFeatures = removeElementFromList(selectedFeatures, currBestFeature);
+            featuresVisited[currBestFeature] = true;
         }
-        returnString += "<b>Selecting best features " + (selectedFeatures) + " was best, accuracy is " + bestAccurancyThisTurn + "</b><br>";
-        console.log("Selecting best features " + (selectedFeatures) + " was best, accuracy is " + bestAccurancyThisTurn + "\n");
+
+        console.log("Selecting best features " + (selectedFeatures) + " with accuracy " + currBestAccuracy);
     }
-    // return selectedFeatures;
-    returnString += "<h3><b>Final best feature for backward elimination is " + (finalBestAttributes) + " and the accuracy is " + maxAccuracy + "</b><h3><br>";
-    console.log("Final best feature for backward elimination is " + (finalBestAttributes) + " and the accuracy is " + maxAccuracy + "\n");
-    return returnString;
+
+    maxAccuracy *= 100;
+    console.log("----------------------------------------------------------------------------------------");
+    console.log("Final best feature for backward elimination is " + (finalBestAttributes) + " and the accuracy is " + (maxAccuracy).toFixed(2) + "%");
+    console.log("----------------------------------------------------------------------------------------");
 }
 
 function calculateAccuracy(index, dataList, localSelectedFeatures) {
-
-    var testerPoint = dataList[index];
-    var minDistanceSquared = Number.MAX_VALUE;
+    // Selecting the data point based on the index provided
+    var givenDataPoint = dataList[index];
+    var minDistSquared = Number.MAX_VALUE;
     var predictedType = 0;
+    // Iterate through each data points
     for (var i = 0; i < dataList.length; i++) {
         if (i == index) {
             continue;
         }
+        var currDistSquared = 0;
+        var currPoint = dataList[i];
 
-        var curDistanceSquared = 0;
-        var candidatePoint = dataList[i];
-
+        //Calculating euclidean distance for givenDataPoint from all other data points
         for (var j of localSelectedFeatures) {
-            curDistanceSquared += (candidatePoint[j] - testerPoint[j]) * (candidatePoint[j] - testerPoint[j]);
+            currDistSquared += (currPoint[j] - givenDataPoint[j]) * (currPoint[j] - givenDataPoint[j]);
         }
-        if (curDistanceSquared < minDistanceSquared) {
-            minDistanceSquared = curDistanceSquared;
-            predictedType = Number.parseInt(candidatePoint[0]);
+        // predict the type by comparing the current DataPoint with all other data points
+        if (currDistSquared < minDistSquared) {
+            minDistSquared = currDistSquared;
+            predictedType = Number.parseInt(currPoint[0]);
         }
     }
-    return Number.parseInt(testerPoint[0]) == predictedType;
+    // Checking if the provided data point type is same as predicted type
+    return Number.parseInt(givenDataPoint[0]) == predictedType;
 }
 
-function calculateAccuracy(index, dataList, localSelectedFeatures) {
-
-    var testerPoint = dataList[index];
-    var minDistanceSquared = Number.MAX_VALUE;
-    var predictedType = 0;
-    for (var i = 0; i < dataList.length; i++) {
-        if (i == index) {
-            continue;
-        }
-
-        var curDistanceSquared = 0;
-        var candidatePoint = dataList[i];
-
-        for (var j of localSelectedFeatures) {
-            curDistanceSquared += (candidatePoint[j] - testerPoint[j]) * (candidatePoint[j] - testerPoint[j]);
-        }
-        if (curDistanceSquared < minDistanceSquared) {
-            minDistanceSquared = curDistanceSquared;
-            predictedType = Number.parseInt(candidatePoint[0]);
-        }
-    }
-    return Number.parseInt(testerPoint[0]) == predictedType;
-}
-
-
+// Utility function to remove provided element from list
 function removeElementFromList(arr, value) {
     return arr.filter(function (temp) {
         return temp != value;
     });
 }
-
-
-readData().then(() => {
-    var strFS = forwardSelection(dataList);
-    var strBE = backwardElimination(dataList);
-    // console.log("strBE:" + strBE);
-
-    var http = require('http');
-    http.createServer(function (req, res) {
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write("<h1 style='text-align:center'><u>CS205 AI: Feature Selection</u></h1>");
-        res.write(strFS);
-        res.write(strBE);
-        res.end();
-    }).listen(8080);
-});
